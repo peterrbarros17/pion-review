ea0a6216a0d942b21894426d7b891d026e79d0d1
peterrbarros17 <peterrbarros17@gmail.com>
2024-03-18 20:37:41 -0300

    feat: add winston for server side rendering logger

---
+++ src/logger.ts
@@ -0,0 +1,17 @@
+import { createLogger, format, transports } from "winston";
+
+const logger = createLogger({
+  level: "info",
+  format: format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new transports.File({ filename: "error.log", level: "error" }),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
+
+export default logger;
